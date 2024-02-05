97c3b43e32bbc60eb2cf411c77b0000332353e6d
ydzervanouskaya <ydzervanouskaya@gmail.com>
2024-02-05 13:31:38 -0500

    test for form layouts created

--- tests/firstTests.spec.ts
+++ tests/firstTests.spec.ts
@@ -2,7 +2,11 @@ import {test} from '@playwright/test';
 
 test.describe('Test suite One', ()=>{
 
-    test('The first test', ()=>{
+    test('The first test', async ({page})=>{
+        await page.goto('http://localhost:4200/')
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+        
 
     })
 })
