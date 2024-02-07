ca323097652a23b21ccf3d9b9cab7e5a9a9a5b6e
ydzervanouskaya <ydzervanouskaya@gmail.com>
2024-02-07 12:42:03 -0500

    locator syntax added

--- tests/locators.spec.ts
+++ tests/locators.spec.ts
@@ -5,3 +5,27 @@ test.beforeEach(async({page})=> {
     await page.getByText('Forms').click()
     await page.getByText('Form Layouts').click()
 })
+
+test('Locator Syntax Rules', async ({page}) => {
+    //by Tag Name
+    page.locator('input')
+
+    // by ID
+   page.locator('#inputEmail1')
+
+  // by class value
+   page.locator('.shape-rectangle')
+
+  //by attribute
+   page.locator('placeholder="Email"')
+
+  //combine different selectors(for example tag and attribute 
+  //!!!important not to put any space btw selectors)
+  page.locator('input[placeholder="Email"]')
+
+  //by partial text match
+ page.locator(':text("Using")')
+
+ // by exact text match
+ page.locator(':text-is("Using the Grid")')
+})
