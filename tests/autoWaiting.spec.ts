0067e2d88ff37dc75478cce80076522c24861d83
ydzervanouskaya <ydzervanouskaya@gmail.com>
2024-03-16 09:36:56 -0400

    additional changes saved

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -5,7 +5,7 @@ test.beforeEach(async({page}) => {
     await page.getByText("Button Triggering AJAX Request").click();
 })
 
-test('Auto-waiting', async ({page}) => {
+test.skip('Auto-waiting', async ({page}) => {
     const button = page.locator('.bg-success')
     //await button.click();
 
@@ -13,20 +13,20 @@ test('Auto-waiting', async ({page}) => {
         expect (text).toEqual('Data loaded with AJAX get request.')
         await button.click();
     })
-test('Auto-wait with .allTextContents() method', async({page}) =>{
+test.skip('Auto-wait with .allTextContents() method', async({page}) =>{
     const button = await page.locator('.bg-success');
     await button.waitFor({state: 'attached'});
     const text = await button.allTextContents();
 
     expect(text).toContain('Data loaded with AJAX get request.')
 })
-test('Auto-wait with {timeout}', async({page}) =>{
+test.skip('Auto-wait with {timeout}', async({page}) =>{
     const button = page.locator('.bg-success');
 
     await expect(button).toHaveText('Data loaded with AJAX get request.', {timeout: 20000});
 
 })
-test('Alternative ways for waits', async({page}) =>{
+test.skip('Alternative ways for waits', async({page}) =>{
     const button = page.locator('.bg-success');
 
     // wait for an element
