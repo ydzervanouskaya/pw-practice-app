daa1e587b01d554efca346aec6f5ef1d16f374dd
ydzervanouskaya <ydzervanouskaya@gmail.com>
2024-03-25 15:18:45 -0400

    Verifying dropdown menu has 4 list items test completed

--- tests/listsAndDropDown.spec.ts
+++ tests/listsAndDropDown.spec.ts
@@ -6,5 +6,14 @@ test.describe('Lists and Drop Down menus', ()=>{
     test('Verifying dropdown menu has 4 list items', async({page}) =>{
         const dropdownMenu = page.locator('ngx-header nb-select');
         await dropdownMenu.click();
+
+        const optionList = page.locator('nb-option-list nb-option');
+        await expect(optionList).toHaveText(['Light','Dark','Cosmic','Corporate'])
     })
+
+    test('Verifying backgroud color change after option selection', async({page})=>{
+        
+    })
+
+
 })
